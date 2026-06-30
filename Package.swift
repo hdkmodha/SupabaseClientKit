7a815a4102629d5dde6edc33472a1ea568d52459
Hardik Modha <hardik.modha27@gmail.com>
2026-06-30 14:13:49 +0530

    setup the supabase client kit

--- Package.swift
+++ Package.swift
@@ -3,8 +3,16 @@
 
 import PackageDescription
 
+extension Target.Dependency {
+    
+    static var supabase: Self {
+        .product(name: "Supabase", package: "supabase-swift")
+    }
+}
+
 let package = Package(
     name: "SupabaseClientKit",
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,11 +20,17 @@ let package = Package(
             targets: ["SupabaseClientKit"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/supabase/supabase-swift.git", .upToNextMajor(from: "2.48.0"))
+        
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SupabaseClientKit"
+            name: "SupabaseClientKit",
+            dependencies: [.supabase]
+            
         ),
         .testTarget(
             name: "SupabaseClientKitTests",
--- Sources/SupabaseClientKit/SupabaseClientKit.swift
+++ Sources/SupabaseClientKit/SupabaseClientKit.swift
@@ -1,2 +1,67 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  SupabaseClientKit.swift
+//  SupabaseClientKit
+//
+//  Created by Hardik Modha on 24/06/26.
+//
+
+import Foundation
+import Supabase
+
+public protocol SupabaseClientProvider: Sendable {
+     var development: SupabaseClient { get }
+     var production: SupabaseClient { get }
+}
+
+
+public final class SupabaseManager: Sendable {
+    
+    let clientProvider: SupabaseClientProvider
+    
+    public init(clientProvider: SupabaseClientProvider) {
+        self.clientProvider = clientProvider
+    }
+    
+    private var client: SupabaseClient {
+        #if DEBUG
+        self.clientProvider.development
+        #else
+        self.clientProvider.production
+        #endif
+    }
+    
+    public func signIn(email: String, password: String) async throws -> String? {
+        let response = try await self.client.auth.signIn(email: email, password: password)
+        return response.user.id.uuidString
+    }
+    
+    public func signUp(withEmail email: String, password: String) async throws -> String? {
+        let response = try await client.auth.signUp(email: email, password: password)
+        let userId = response.user.id.uuidString
+        return userId
+    }
+    
+    public func signIn(withPhone phoneNumber: String, password: String) async throws -> String? {
+        let response = try await self.client.auth.signIn(phone: phoneNumber, password: password)
+        let userId = response.user.id.uuidString
+        return userId
+    }
+    
+    public func signUp(withPhone phoneNumber: String, password: String) async throws -> String? {
+        let response = try await client.auth.signUp(phone: phoneNumber, password: password)
+        let userId = response.user.id.uuidString
+        return userId
+    }
+    
+    public func signInWithIdToken(withToken idToken: String, accessToken: String, provider: OpenIDConnectCredentials.Provider) async throws -> Session {
+        let credenital = OpenIDConnectCredentials(
+            provider: provider,
+            idToken: idToken,
+            accessToken: accessToken
+        )
+        let session = try await self.client.auth.signInWithIdToken(credentials: credenital)
+        return session
+    }
+}
+
+
